843f9b5a143613d496c54ae0bc7c8adaaa146192
CaioGMorata <51411417+CaioGMorata@users.noreply.github.com>
2024-12-01 20:26:50 -0300

    Update Exercise_10.cpp

--- Exercise_10.cpp
+++ Exercise_10.cpp
@@ -3,21 +3,29 @@ using namespace std;
 
 int main() {
     //Variável para armazenar a string
+
+    //Variable to store the string
     string input;
 
     // Entrada da string via teclado
+
+    // String input via keyboard
     cout << "Enter a string: ";
     getline(cin, input); 
 
     // Inicialização
+
+    // Initialization
     int length = 0;
 
     // Loop através de cada caracter
+
+    // Loop through each character
     while (input[length] != '\0') {
-        length++; // Incremento de cada caracter
+        length++; // Incremento de cada caracter. Increment of each character
     }
 
-    // Saída na tela
+    // Saída na tela. Output in the screen.
     cout << "The length of the string is: " << length << endl;
 
     return 0;
